e250af48e07de99032201e2622d31a2716046fec
SamSswi <sadovoisamuel7@gmail.com>
2023-06-20 13:42:51 +0000

    Create a project summary page

---
+++ app_pages/project_summary_page.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import matplotlib.pyplot as pyplot
+
+def project_summary_page_body():
+    st.subheader('Quick Project Summary')
+
+    st.info(
+        '###### General Information \n'
+        '* Cherry leaf powdery mildew, also known as cherry powdery mildew or cherry leaf blight, is a fungal disease that affects various species of cherry trees, including sweet cherries (Prunus avium) and sour cherries (Prunus cerasus). \n'
+        '* On leaves, powdery mildew appears as patches of white, powdery or felt-like fungal growth. Severely affected leaves and shoots are often puckered or distorted.\n'
+        '* Season long disease control is critical to minimize overall disease pressure in the orchard and consequently to protect developing fruit from accumulating spores on their surfaces.\n'
+        '###### Project Dataset \n'
+        '* The available dataset contains 4208 images of healthy cherry tree leaves and cherry tree leaves infected with powdery mildew.'
+    )
+
+    st.warning(
+        '* For additional information, please visit and **read** the [Project README file](https://malaria-predictor.onrender.com/)'
+    )
+
+    st.success(
+        '###### Business Requirements \n'
+        '* The client is interested in conducting a study to visually differentiate a cherry leaf that is healthy from one that contains powdery mildew. \n'
+        '* The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew.\n')
